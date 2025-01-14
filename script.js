944b7d9015bb3851a2437b76b8ee42f9bbfcfb1f
odys <96631467+odys1213@users.noreply.github.com>
2025-01-14 17:40:37 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+let clickCount = 0;
 
+const clickerButton = document.getElementById('clicker');
+const clickCountDisplay = document.getElementById('clickCount');
+
+clickerButton.addEventListener('click', () => {
+    clickCount++;
+    clickCountDisplay.textContent = `Количество кликов: ${clickCount}`;
+    sendClickData(clickCount);
+});
+
+// Функция для отправки кликов на сервер
+function sendClickData(count) {
+    fetch('http://localhost:5000/click', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ clickCount: count })
+    })
+    .then(response => response.json())
+    .then(data => console.log('Данные отправлены на сервер:', data))
+    .catch(error => console.error('Ошибка при отправке данных:', error));
+}
